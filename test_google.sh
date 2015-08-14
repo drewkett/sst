f9fa63769b2dd592c57f11ddf2602c1dd583bd9c
Andrew Burkett <burkett.andrew@gmail.com>
2015-08-14 12:05:21 -0700

    Make exit code return the number of errors

--- test_google.sh
+++ test_google.sh
@@ -4,4 +4,5 @@ echo "Testing http://www.google.com http code"
 code=`curl -s -o /dev/null -I -w "%{http_code}" http://www.google.com`
 if [ $code -ne "200" ]; then
 	echo "google.com http code != 200"
+	exit 1
 fi
