ae984b95fbf1484be1dede9e739b75f34b7d512d
Andrew Burkett <burkett.andrew@gmail.com>
2015-08-14 12:01:02 -0700

    Add message to test_google.sh

--- test_google.sh
+++ test_google.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "Testing http://www.google.com http code"
 code=`curl -s -o /dev/null -I -w "%{http_code}" http://www.google.com`
 if [ $code -ne "200" ]; then
 	echo "google.com http code != 200"
